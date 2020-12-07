7f6c13a6a5cf4e4bfe007beef45a429cc7090679
Caio Oliveira <caio.oliveira@olxbr.com>
2020-12-07 20:31:11 -0300

    Init project with Hello world

---
+++ StockAlarmRobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockAlarmRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
